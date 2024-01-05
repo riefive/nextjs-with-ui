4a0b9df2334a1e79ab5a4a0ef50bfff5f0e08b31
riefive <muhammad.rifai@salt.co.id>
2024-01-05 12:52:42 +0700

    chore: introduced next ui

---
+++ src/components/commons/ProvidersNextUi.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+
+export function ProvidersNextUi(
+    { children }: { children: React.ReactNode }
+) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
